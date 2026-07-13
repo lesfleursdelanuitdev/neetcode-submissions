602506e3d424ae11014658b517e6f3d06d87aa73
lesfleursdelanuitdev <lesfleursdelanuit.dev@gmail.com>
2026-07-13 07:12:27 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # determine the character distribution for each string 
+        charDist = {}
+        charDist["s"] = self.determineCharDistribution(s)
+        charDist["t"] = self.determineCharDistribution(t)
+
+        if not len(charDist["s"].keys()) == len(charDist["t"].keys()):
+            return False 
+
+        for k in charDist["s"].keys():
+            if k not in charDist["t"].keys():
+                return False
+
+            if not charDist["t"][k] == charDist["s"][k]:
+                return False
+
+        return True  
+
+    def determineCharDistribution(self, s: str) -> dict: 
+        seen = {}
+        for c in s: 
+            if c not in seen: 
+                seen[c] = 0 
+
+            seen[c] += 1
+
+        return seen 
