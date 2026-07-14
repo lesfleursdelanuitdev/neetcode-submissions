08f68153ff186d75f3e1d67268c8201d0ca6f8b4
lesfleursdelanuitdev <lesfleursdelanuit.dev@gmail.com>
2026-07-14 13:38:52 -0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0 
+        r = len(s) - 1 
+        while l < r: 
+            # are both chars valid? 
+            l = self.getLeftIndex(s,l)
+            r = self.getRightIndex(s,r)
+            # if you get here, both chars should be alphanumeric 
+            if l < len(s) and r >= 0 and not s[l].lower() == s[r].lower():
+                return False
+
+            l += 1 
+            r -= 1
+
+        return True
+
+    def getLeftIndex(self, s, left):
+        # keep moving forward until you find an alphanumeric character 
+        # or reach the end of the string. Once either condition is met, 
+        # return the value of left
+        while left < len(s) and not self.isAlphanumeric(s[left].lower()):
+            left += 1
+        return left 
+
+    def getRightIndex(self,s, right):
+        # Keep moving backwards until you find an alphanumeric character 
+        # or reach the beginning of the string. Once either condition is met, 
+        # return right 
+        while right >= 0 and not self.isAlphanumeric(s[right].lower()):
+            right -= 1 
+
+        return right
+
+    def isAlphanumeric(self, c):
+        return "0" <= c <= "9" or "a" <= c <= "z"
