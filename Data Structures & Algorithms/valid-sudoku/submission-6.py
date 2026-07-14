a1a1bc66da1fbe846a1b741107bee176356c110c
lesfleursdelanuitdev <lesfleursdelanuit.dev@gmail.com>
2026-07-14 12:08:12 -0700

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.py
@@ -0,0 +1,49 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rowSeen = {i: set() for i in range(9)}
+        colSeen = {i: set() for i in range(9)}
+        for i in range(9):
+            for j in range(0,9):
+                cell = board[i][j]
+                if not self.validChar(cell):
+                    return False
+                elif not cell == '.' and cell in rowSeen[i]: 
+                    return False 
+                elif not cell == '.' and cell in colSeen[j]:
+                    return False 
+
+                rowSeen[i].add(cell)
+                colSeen[j].add(cell)
+
+        r = (0,3)
+        while r[0] < 9: 
+            c = (0,3)
+            while c[0] < 9: 
+                if not self.validSquare(r,c,board):
+                    return False
+                c = (c[0] + 3, c[1]+3)
+            r = (r[0] + 3, r[1] + 3)
+
+        return True
+
+    def validSquare(self, r: (int,int), c: (int,int), board: List[List[str]]) -> bool:
+        return self.validDim(r,c,board)
+
+    def validDim(self, r: (int,int), c: (int,int), board: List[List[str]]) -> bool:
+        seen = {}
+        for i in range(r[0],r[1]):
+            for j in range(c[0],c[1]):
+                cell = board[i][j]
+                if not self.validChar(cell):
+                    return False 
+                if cell in seen and not cell == '.':
+                    return False
+                seen[cell] = True
+        return True
+
+    def validChar(self, value: str) -> bool:
+        return self.isDigit(value) or value == '.'
+
+    def isDigit(self, value: str) -> bool:
+        valid = len(value) == 1 and "0" <= value <= "9"
+        return valid
